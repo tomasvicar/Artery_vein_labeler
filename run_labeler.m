d02a4a8021a332bf85f1621b7917bf9149bf52f1
Tomáš Vičar <vicar@PC-SE4158-716.ubmi.feec.vutbr.cz>
2021-04-27 00:15:39 +0200

    fgfgf

--- run_labeler.m
+++ run_labeler.m
@@ -10,13 +10,13 @@ mask_name = replace(replace(img_name,'images','manual1'),'.JPG','.tif');
 artery_vein_labeler(img_name,mask_name,user_name)
 
 
-
-
 % malé cucky neřešte - to se pak kdyžtak nějak automaticky dočistí ;)
 % zkratky jsou napsané v závorkách
 % join - spojí objekty kterých se kreslením dotknete
 % split - rozdělí nakreslenou čárou
 % save a load můžete využít pro průběžné uložení
 % ok provede uložení - soubor najdete u dat...
-
+% dělal sem to v 2021a matlabu, tak bude asi potřeba
+% cévy sem trochu dilatoval aby to bylo líp vidět (to pak vrátím zpátky) 
+% zmenšil sem obrázek na polovinu, aby to jelo trochu rychleji - i tak to není uplně super rychlé ale nenapadá mě jak to udělat líp
 
